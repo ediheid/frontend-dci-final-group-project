b389fabff1497d84d460664e6d52b3c599d277a2
Edith-Heidmann <eheidmann@gmail.com>
2021-12-09 11:16:15 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+// Main scss
 import "./Styling/app.scss";
 
 const App = () => {
   return (
-    <div className="app-container">This is gonna be our awesome app!!</div>
+    <div className="app-container">
+      This is gonna be our awesome app!!
+      <Router>
+        {/* // !! NavBar Component can live here */}
+        <main>
+          <Switch>
+            {/* // !! Template/placeholder for how to setup paths with components.. */}
+            {/*  <Route path="/" exact component={Home} />*/}
+
+            {/* // !! Fallback path - directs user back to login page */}
+            {/* <Redirect to="/" exact /> */}
+            {/* // !! or */}
+            {/* <Route path="*" exact /> */}
+          </Switch>
+        </main>
+
+        {/* // !! Footer Component can live here */}
+      </Router>
+    </div>
   );
 };
 
