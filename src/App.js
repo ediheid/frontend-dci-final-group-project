0d1ee4bb829ebecc57a4a8484db5d40452a5d47e
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2021-12-13 15:07:28 +0100

    bugfixing for Jamie

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch,
 } from "react-router-dom";
+
+import LandingPage from "./Views/LandingPage/LandingPage";
 
 // ? Main scss
 
 // !!!! - Figure out module classes..
-import styles from "./Styling/App.module.scss";
+// import styles from "./Styling/App.module.scss";
 
 const App = () => {
-  return (
-    // !!!! - Figure out module classes..
-    <div>
-      <Router>
-        {/* <Navbar /> */}
-        {/* // ? NavBar Component could live here */}
-        <main>
-          <Switch>
-            {/* // ? Template/placeholder for how to setup paths with components.. */}
-            {/*  <Route path="/" exact component={Home} />*/}
+    return (
+        // !!!! - Figure out module classes..
+        <div>
+            <Router>
+                {/* <Navbar /> */}
+                {/* // ? NavBar Component could live here */}
+                <main>
+                    <Switch>
+                        {/* // ? Template/placeholder for how to setup paths with components.. */}
+                        {/*  <Route path="/" exact component={Home} />*/}
+                        <LandingPage />
 
-            {/* // ? Fallback path - directs user back to login page */}
-            {/* <Redirect to="/" exact /> */}
-            {/* // ? or */}
-            {/* <Route path="*" exact /> */}
-          </Switch>
-        </main>
-        {/* <Footer /> */}
-        {/* // ? Footer  Component could live here */}
-      </Router>
-    </div>
-  );
+                        {/* // ? Fallback path - directs user back to login page */}
+                        {/* <Redirect to="/" exact /> */}
+                        {/* // ? or */}
+                        {/* <Route path="*" exact /> */}
+                    </Switch>
+                </main>
+                {/* <Footer /> */}
+                {/* // ? Footer  Component could live here */}
+            </Router>
+        </div>
+    );
 };
 
 export default App;
--- src/Views/LandingPage/LandingPage.js
+++ src/Views/LandingPage/LandingPage.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import styles from "../LandingPage/LandingPage.module.scss";
 
 const LandingPage = () => {
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <div className={styles.container}>
+            <div className={styles["about-us"]}>ABOUT US</div>
+            <div className={styles.carousel}>Carousel</div>
 
-export default LandingPage
+            <div className={styles.info}>
+                <div className={styles["info-guest"]}>GUEST</div>
+                <div className={styles["info-host"]}>HOST</div>
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
