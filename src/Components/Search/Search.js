730243ca1d1e565eef3bd49504a37677ede8303f
Edith-Heidmann <eheidmann@gmail.com>
2021-12-14 22:08:54 +0100

    Styled react-dropdown to be in line with the main styling for search items in the sass file - based off of react-dropdown library styles - still needs to be refactored to Sass with correct units. Moved folder into Search folder and imported directly from there to make sure that default styles do not overwrite updated styles. Added dummy code to implement function to receive filter items from the backend

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -4,22 +4,35 @@ import styles from "../Search/search.module.scss";
 
 // Libraries
 import Expand from "react-expand-animated";
+import Dropdown from "react-dropdown";
+
+// ? Updated this style sheet!
+// import "react-dropdown/style.css";
+// todo to this.. (still need to refactor to SASS with correct units)
+import "../Search/dropdown-styling.css";
 
 const Search = () => {
-  // Set state
+  // State hooks
   const [open, setOpen] = useState(false);
 
-  // toggle
+  // Open Search function
   const openSearch = () => {
-    // ! Question for team: Should we have it as a toggle OR have state as true so dropdown only closes on submit?
-    setOpen(!open);
+    // !! Question for team: Should we have it as a toggle OR have state as true so dropdown only closes on submit?
+    setOpen(true);
+    // ! OR
+    // setOpen(!open);
   };
+
+  // ! Dropdown - Ready to take in data
+  const options = ["one", "two", "three"];
+  // ? Default
+  // const defaultOption = options[0]; // ? Maybe add a placeholder instead?
 
   return (
     <header className={styles["search-main-container"]}>
       {/* // todo Do I actually need this form container? */}
       <div className={styles["form-container"]}>
-        {/* // ! Note: Start of Expand for dropdown search fields - expand sits WITHIN the form so everything can be submitted at once (onSubmit) */}
+        {/* // todo Note: Start of Expand for dropdown search fields - expand sits WITHIN the form so everything can be submitted at once (onSubmit) */}
         <Fragment>
           {/* // ! Note: onSubmit placeholder for collection data in future */}
           {/* // todo - future function will reset form AND setOpen to FALSE */}
@@ -37,7 +50,7 @@ const Search = () => {
                 <div className={styles["search-item"]}>
                   <label className={styles["search-labels"]}>Check in</label>
                   <input
-                    className={styles["dropdown-input"]}
+                    className={styles["dropdown-section-input"]}
                     placeholder="Add dates"
                   ></input>
                 </div>
@@ -46,7 +59,7 @@ const Search = () => {
                 <div className={styles["search-item"]}>
                   <label className={styles["search-labels"]}>Check out</label>
                   <input
-                    className={styles["dropdown-input"]}
+                    className={styles["dropdown-section-input"]}
                     placeholder="Add dates"
                   ></input>
                 </div>
@@ -55,20 +68,23 @@ const Search = () => {
                 <div className={styles["search-item"]}>
                   <label className={styles["search-labels"]}>Campervans</label>
                   <input
-                    className={styles["dropdown-input"]}
+                    className={styles["dropdown-section-input"]}
                     placeholder="e.g. 1"
                   ></input>
                 </div>
 
-                {/* Dropdown for amenities will go here.. */}
+                {/* Dropdown for amenities */}
                 <div className={styles["search-item"]}>
-                  <label className={styles["search-labels"]}>Amenities</label>
-                  <input className={styles["dropdown-input"]}></input>
+                  <label className={styles["search-labels"]}>Extras</label>
+                  <Dropdown
+                    // ? Styled in "../Search/dropdown-styling.css" - from node modules default styles;
+                    // className={styles["dropdown-style"]}
+                    options={options}
+                    // onChange={this._onSelect}
+                    // value={defaultOption}
+                    // placeholder="Looking for?"
+                  />
                 </div>
-                {/* Drop down of hidden extra input on form */}
-                {/* Will it be on enter? */}
-                {/* Manually code it instead of bringing in library */}
-                {/* // !!! Collapsible div will sit here and open on search - */}
               </div>
             </Expand>
           </form>
