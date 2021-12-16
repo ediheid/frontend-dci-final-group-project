4dbd8894b3230cf960350f3715b74eda71be3ea8
Janowitz <janhapple@outlook.de>
2021-12-16 11:15:57 +0100

    build up verification page

--- src/Components/Verification/Verification.js
+++ src/Components/Verification/Verification.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Verification.module.scss";
 import Button from "../../UI/Button/Button.js";
+import { Link } from "react-router-dom";
 
 const useQuery = () => {
     return new URLSearchParams(window.location.search).get("message")
@@ -8,12 +9,28 @@ const useQuery = () => {
 
 const Verification = () => {
     const query = useQuery();
+  
+    const changeContent = () => {
+        if (query === "Your account has been successfully activated!" || query === "User has already been verified. Please login!") {
+            return (<Link to="/login">
+                        <Button>Login</Button>
+                    </Link>)
+        } else if (query === "We were unable to find a user for this verification. Please SignUp!") {
+            return (<Link to="/signup">
+                        <Button>Login</Button>
+                    </Link>)
+        } else {
+            return (<Link to="/login">
+                        <a href="#">resend confirmation mail</a>
+                    </Link>)
+        }
+    }
 
     return(
         <main className={styles.main}>
             <div className={styles.msgbox}>
-                <h1 className={styles.heading}>{query}</h1>
-                <Button>Login</Button>
+                <h1 className={styles.heading} id="verification">{query}</h1>
+                {changeContent()}
             </div>    
         </main>
     )
