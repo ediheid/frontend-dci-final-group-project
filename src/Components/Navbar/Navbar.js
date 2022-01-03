94bacffc147ec8880e40e5b7947907a9e469f1c3
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2022-01-03 15:08:42 +0100

    closing modal and signup works

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -15,13 +15,14 @@ const Navbar = (props) => {
         setShowLoginModal(true);
     };
 
-    const openLoginFromSignup = () => {
+    const openLoginFromSignup = (event) => {
         setShowSignupModal(false);
         setShowLoginModal(true);
     };
 
     const closeLoginModal = (event) => {
         event.stopPropagation();
+
         event.preventDefault();
         setShowLoginModal(false);
     };
@@ -34,6 +35,7 @@ const Navbar = (props) => {
 
     const closeSignupModal = (event) => {
         event.stopPropagation();
+
         event.preventDefault();
         setShowSignupModal(false);
     };
@@ -88,65 +90,3 @@ const Navbar = (props) => {
 };
 
 export default Navbar;
-
-///////////////////////////////////////////////////////////////////
-// import React, { useState } from "react";
-
-// // ? Stylesheet
-// import styles from "../Navbar/Navbar.module.scss";
-
-// // ? All Component and View imports
-// import LogIn from "../../Components/LogIn/LogIn";
-// import SignUp from "../../Components/SignUp/SignUp";
-
-// const Navbar = () => {
-//     const [showLogIn, setShowLogIn] = useState(false);
-
-//     const openLogIn = (event) => {
-//         event.preventDefault();
-//         setShowLogIn(true);
-//     };
-
-//     const closeLogIn = (event) => {
-//         event.preventDefault();
-//         setShowLogIn(false);
-//         console.log("background was clicked close log in");
-//     };
-
-//     return (
-//         <>
-//             <div className={styles["navbar-container"]}>
-//                 <LogIn
-//                     onCloseLogin={closeLogIn}
-//                     showLogin={showLogIn}
-//                     // setShowLogIn={setShowLogIn}
-//                 />
-
-//                 {/* <SignUp onCloseSignup={closeSignUp} showSignup={showSignUp} /> */}
-//                 <div className={styles["icon-container"]}>
-//                     <div className={styles.icon}>
-//                         <i class="fas fa-caravan"></i>
-//                     </div>
-
-//                     <div className={styles.icon}>
-//                         <i class="fas fa-search"></i>
-//                     </div>
-
-//                     <div className={styles.icon}>
-//                         <i class="far fa-heart"></i>
-//                     </div>
-
-//                     <div className={styles.icon}>
-//                         <i class="far fa-comment"></i>
-//                     </div>
-
-//                     <div className={styles.icon} onClick={openLogIn}>
-//                         <i class="far fa-user-circle"></i>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
-
-// export default Navbar;
