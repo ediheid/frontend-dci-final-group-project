d8e1e6094a32b283f1c8568d4e3cab99862856f0
Edith-Heidmann <eheidmann@gmail.com>
2022-01-06 22:01:49 +0100

    Added location-box placement across all devices

--- src/Components/Map/LocationInfoBox.js
+++ src/Components/Map/LocationInfoBox.js
@@ -5,21 +5,23 @@ import styles from "../Map/Map.module.scss";
 const LocationInfoBox = ({ info }) => {
   return (
     <div className={styles["location-box"]}>
-      <h2>Event Location Info</h2>
-      <ul>
-        {/* <li>
+      <div className={styles["inner-location-box"]}>
+        <h2>Event Location Info</h2>
+        <ul>
+          {/* <li>
           ID: <strong>{info.id}</strong>
         </li> */}
-        <li>
-          <strong>{info.title}</strong>
-        </li>
-      </ul>
-      <img
-        className={styles["img-container"]}
-        alt={info.title}
-        src={info.img}
-      ></img>
-      <h3>LINK TO PROPERTY PAGE!</h3>
+          <li>
+            <strong>{info.title}</strong>
+          </li>
+        </ul>
+        <img
+          className={styles["img-container"]}
+          alt={info.title}
+          src={info.img}
+        ></img>
+        <h3>LINK TO PROPERTY PAGE!</h3>
+      </div>
     </div>
   );
 };
