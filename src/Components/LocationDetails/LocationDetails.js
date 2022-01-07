2c093c6d28f52930f88675365f12ba876902efcb
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2022-01-07 22:16:29 +0100

    added more content. next step: build modal to open a more detailed description and see more amnenities

--- src/Components/LocationDetails/LocationDetails.js
+++ src/Components/LocationDetails/LocationDetails.js
@@ -8,6 +8,7 @@ import styles from "../LocationDetails/LocationDetails.module.scss";
 // ? All Component and View imports
 import Search from "../Search/Search";
 import Navbar from "../Navbar/Navbar";
+import Button from "../../UI/Button/Button";
 
 // ? All Images
 import mainImage from "../LocationDetails/static/pexels-mali-maeder-109679.jpg";
@@ -36,22 +37,19 @@ const LocationDetails = () => {
                     <div className={styles.heading}>
                         Lonely place in the middle of black forest
                     </div>
-
                     {/* // !! Address */}
                     <div className={styles.location}>
                         Feldberg, Baden-Würtemberg, Germany
                     </div>
-
                     <hr className={styles.hr} />
-
                     {/* // !! Info */}
-                    <div className={styles.info}>This is a rare find.</div>
-
+                    <div className={styles.info}>
+                        This is a rare find. Lucky you!
+                    </div>
                     <hr className={styles.hr} />
-
                     <div className={styles["intro-container"]}>
                         {/* // !! Title + host + intro */}
-                        <div className={styles.intro}>
+                        <div className={styles.heading}>
                             Lonely place <br /> hosted by Florian
                         </div>
 
@@ -63,7 +61,6 @@ const LocationDetails = () => {
                             />
                         </div>
                     </div>
-
                     {/* // !! Title + host + intro */}
                     <div className={styles["intro-details"]}>
                         <div>1 caravan</div>
@@ -72,8 +69,103 @@ const LocationDetails = () => {
                         <div>no shower</div>
                         <div>animals allowed</div>
                     </div>
+                    <hr className={styles.hr} />
+
+                    {/* // !! Nice to have */}
+                    <div className={styles["bonus-container"]}>
+                        <div className={styles.icon}>
+                            <i class="fas fa-shuttle-van"></i>
+                        </div>
+                        <div className={styles["bonus-description"]}>
+                            <div>Entire home</div>
+                            <div>You'll have the place to yourself.</div>
+                        </div>
+                    </div>
+
+                    <div className={styles["bonus-container"]}>
+                        <div className={styles.icon}>
+                            <i class="fas fa-door-closed"></i>
+                        </div>
+                        <div className={styles["bonus-description"]}>
+                            <div>Self check-in</div>
+                            <div>Check yourself in.</div>
+                        </div>
+                    </div>
+
+                    <div className={styles["bonus-container"]}>
+                        <div className={styles.icon}>
+                            <i class="fas fa-mobile-alt"></i>
+                        </div>
+                        <div className={styles["bonus-description"]}>
+                            <div>Great communication experience</div>
+                            <div>The host replays in less then 1 hour.</div>
+                        </div>
+                    </div>
+
+                    <div className={styles["bonus-container"]}>
+                        <div className={styles.icon}>
+                            <i class="far fa-calendar-times"></i>
+                        </div>
+                        <div className={styles["bonus-description"]}>
+                            <div>Free cancellation for 48 hours</div>
+                            <div>{""}</div>
+                        </div>
+                    </div>
 
                     <hr className={styles.hr} />
+
+                    <div className={styles["detailed-description"]}>
+                        Lemon drops candy canes marshmallow cake apple pie.
+                        Toffee wafer bear claw jujubes liquorice chupa chups
+                        cotton candy gummies. Apple pie cookie jujubes jujubes
+                        pastry halvah. <br />
+                        <br /> Toffee candy canes chocolate cake sesame snaps
+                        marzipan oat cake bear claw chocolate. Jelly-o macaroon
+                        bear claw apple pie oat cake halvah. Chupa chups sesame
+                        snaps brownie marshmallow topping sesame snaps pie.{" "}
+                        <div className={styles["modal-description-link"]}>
+                            Show more >
+                        </div>
+                    </div>
+
+                    <hr className={styles.hr} />
+
+                    <div className={styles.heading}>What this place offers</div>
+
+                    <div className={styles["bonus-container"]}>
+                        <div className={styles.icon}>
+                            <i class="fas fa-tree"></i>{" "}
+                        </div>
+                        <div className={styles["bonus-description"]}>
+                            <div>Forest view</div>
+                            <div>{""}</div>
+                        </div>
+                    </div>
+
+                    <div className={styles["bonus-container"]}>
+                        <div className={styles.icon}>
+                            <i class="fas fa-box-open"></i>
+                        </div>
+                        <div className={styles["bonus-description"]}>
+                            <div>Welcome package</div>
+                            <div>{""}</div>
+                        </div>
+                    </div>
+
+                    <div className={styles["bonus-container"]}>
+                        <div className={styles.icon}>
+                            <i class="fas fa-campground"></i>{" "}
+                        </div>
+                        <div className={styles["bonus-description"]}>
+                            <div>Calmness</div>
+                            <div>{""}</div>
+                        </div>
+                    </div>
+                    <Button>Show all amenities</Button>
+
+                    <hr className={styles.hr} />
+
+                    <div className={styles.heading}>Where you'll be</div>
                 </div>
             </div>
         </>
