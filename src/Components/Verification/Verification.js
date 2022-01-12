7a29e0a0d01a3ec8bb3a2609ca06a20629111dbe
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2022-01-12 14:29:11 +0100

    changed checkMail and Verification styling

--- src/Components/Verification/Verification.js
+++ src/Components/Verification/Verification.js
@@ -6,59 +6,63 @@ import Navbar from "../Navbar/Navbar";
 import { AppContext } from "../../App";
 
 const useQuery = () => {
-  return new URLSearchParams(window.location.search).get("message");
+    return new URLSearchParams(window.location.search).get("message");
 };
 
 const Verification = () => {
-  const query = useQuery();
+    const query = useQuery();
 
-  const verificationContext = useContext(AppContext);
+    const verificationContext = useContext(AppContext);
 
-  const showLoginModal = () => {    
-    verificationContext.setShowLoginModal(true);
-  };
+    const showLoginModal = () => {
+        verificationContext.setShowLoginModal(true);
+    };
 
-  const changeContent = () => {
-    if (
-      query === "Your account has been successfully activated!" ||
-      query === "User has already been verified. Please login!"
-    ) {
-      return (
-        <Link to="/" onClick={showLoginModal}>
-          <Button>Login</Button>
-        </Link>
-      );
-    } else if (
-      query ===
-      "We were unable to find a user for this verification. Please SignUp!"
-    ) {
-      return (
-        <Link to="/signup">
-          <Button>Sign up</Button>
-        </Link>
-      );
-    } else {
-      return (
-        <Link to="/login">
-          <a href="#">resend confirmation mail</a>
-        </Link>
-      );
-    }
-  };
+    const changeContent = () => {
+        if (
+            query === "Your account has been successfully activated!" ||
+            query === "User has already been verified. Please login!"
+        ) {
+            return (
+                <Link
+                    to="/"
+                    onClick={showLoginModal}
+                    className={styles["button-container"]}
+                >
+                    <Button>Login</Button>
+                </Link>
+            );
+        } else if (
+            query ===
+            "We were unable to find a user for this verification. Please SignUp!"
+        ) {
+            return (
+                <Link to="/signup">
+                    <Button>Sign up</Button>
+                </Link>
+            );
+        } else {
+            return (
+                <Link to="/login" className={styles.heading}>
+                    <a href="#">Resend confirmation mail</a>
+                </Link>
+            );
+        }
+    };
 
-  return (
-    <>
-      <Navbar />
-      <main className={styles.main}>
-        <div className={styles.msgbox}>
-          <h1 className={styles.heading} id="verification">
-            {query}
-          </h1>
-          {changeContent()}
-        </div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <main className={styles["main-container"]}>
+                <div className={styles["content-container"]}>
+                    <div className={styles.heading} id="verification">
+                        {query}
+                    </div>
+                    {changeContent()}
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default Verification;
