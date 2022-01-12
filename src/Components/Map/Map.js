1dde8ea73f7f6bec200089eb6189d07e67706342
Edith-Heidmann <eheidmann@gmail.com>
2022-01-12 16:20:52 +0100

    New changes in main

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -135,26 +135,17 @@ const Map = ({ center, zoom }) => {
       </button>
 
       <GoogleMapReact
-        // styles={mapStyles.mapStyles}
-        // style={{  }}
-        // options={{ styles: mapStyles.styles }}
-        // styles={mapStyles.styles}
-        // styles={mapStyles.mapStyles}
-        // options={{ styles: mapStyles.mapStyles }}
-        // styles={styles.styles}
-        // styles={mapStyles}
-
+        // !!! - Snazzy Maps only works when API active
         options={{ styles: mapStyles }}
-        // !!!!
         // ? Added close location box on map so - this works on map but user can still click on a Marker and it will open!
         onClick={mapContext.closeLocationInfoBox}
         className={styles["map-component"]}
         // !!! API key lives in .env file - when commented out Map runs in dev mode
         // !!! Use like this so we don't use up the API
-        bootstrapURLKeys={{
-          key: MAP_API,
-          language: "en",
-        }}
+        // bootstrapURLKeys={{
+        //   key: MAP_API,
+        //   language: "en",
+        // }}
         defaultCenter={center}
         center={center}
         defaultZoom={zoom}
@@ -175,11 +166,6 @@ const Map = ({ center, zoom }) => {
 // !!! Map Component default props live here..
 
 Map.defaultProps = {
-  // styles: require("../../Styling/snazzy-map-avocado.json"),
-  //  styles={mapStyles.styles},
-  //  styles: {mapStyles.styles},
-  // styles:{ mapStyles.styles },
-
   // Center coordinates set to Baden-Württemberg
   center: { lat: 48.758339, lng: 8.243008 },
   defaultCenter: { lat: 48.758339, lng: 8.243008 },
