b84165162df514c21b612b90c6bc5082572a588e
Edith-Heidmann <eheidmann@gmail.com>
2022-01-12 12:32:09 +0100

    Added scroll to top functionality across app for view changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Redirect,
   Switch,
   useHistory,
+  useLocation,
 } from "react-router-dom";
 import signup from "./Services/createNewUser.js";
 import Cookies from "js-cookie";
@@ -14,6 +15,7 @@ import { useCookies } from "react-cookie";
 import styles from "./Styling/app.module.scss";
 
 // ? All Component and View imports
+import ScrollToTop from "./Components/ScrollToTop.js";
 import LandingPage from "./Views/LandingPage/LandingPage";
 import AboutUs from "./Views/AboutUs/AboutUs";
 import Footer from "./Components/Footer/Footer";
@@ -200,10 +202,12 @@ const App = () => {
       >
         {/* NAVBAR and Search Components live in respective View Components */}
         <main>
+          <ScrollToTop />
           <Switch>
             {/* // ? Template/placeholder for how to setup paths with components.. */}
             {/* <Route path="/" exact component={LandingPage} /> */}
             <Route exact path="/" component={LandingPage} />
+
             {/* {cookies.UserCookie !== "null" ?
                 <Redirect to="/welcome-page" />  */}
             {/* :  */}
@@ -228,108 +232,6 @@ const App = () => {
       </AppContext.Provider>
     </div>
   );
-  //   const [loginData, setLoginData] = useState({
-  //     email: "",
-  //     password: "",
-  //   });
-  //   const [currentUser, setCurrentUser] = useState({
-  //     _id: "",
-  //     firstname: "",
-  //     lastname: "",
-  //     email: "",
-  //     adress: {
-  //       street: "",
-  //       number: "",
-  //       city: "",
-  //       postcode: "",
-  //     },
-  //     birthday: "",
-  //     locations: [],
-  //     bookings: [],
-  //     verified: false,
-  //   });
-
-  //   const collectSignupData = (event) => {
-  //     setSignupData({
-  //       ...signupData,
-  //       [event.target.name]: event.target.value,
-  //     });
-  //   };
-
-  //   const collectLoginData = (event) => {
-  //     setLoginData({ ...loginData, [event.target.name]: event.target.value });
-  //   };
-
-  //   const [showLoginModal, setShowLoginModal] = useState(false);
-  //   const [showSignupModal, setShowSignupModal] = useState(false);
-
-  //   // console.log("!!!!!!!", loginData);
-
-  //   // console.log("??????", signupData);
-
-  //   return (
-  //     <div>
-  //       {/* // !!! This is where our context lives */}
-  //       <AppContext.Provider
-  //         value={{
-  //           // ? Collect signup and login data context
-  //           collectSignupData: collectSignupData,
-  //           collectLoginData: collectLoginData,
-
-  //           // ? Search Context to pass down to Search and Navbar..
-  //           openSearch: openSearch,
-  //           openForm: openForm,
-  //           closeSearchButton: closeSearchButton,
-
-  //           // ? Map Context
-  //           mapView: mapView,
-  //           openMap: openMap,
-
-  //           // ? Sign up and login Context
-  //           setShowSignupModal: setShowSignupModal,
-  //           setShowLoginModal: setShowLoginModal,
-  //           showLoginModal: showLoginModal,
-  //           showSignupModal: showSignupModal,
-
-  //           signupData: signupData,
-  //           setSignupData: setSignupData,
-  //           loginData: loginData,
-  //           setLoginData: setLoginData,
-
-  //           // !!! Map test..
-  //           // ! Not sure if we will use loader or not? as it may interfere with already existing conditional rendering on the map from Form
-  //           // mapLoading: mapLoading,
-  //           // mapEventData: mapEventData,
-  //           // events: events,
-  //         }}
-  //       >
-  //         <Router>
-  //           {/* NAVBAR and Search Components live in respective View Components */}
-  //           <main>
-  //             <Switch>
-  //               {/* // ? Template/placeholder for how to setup paths with components.. */}
-  //               <Route path="/" exact component={LandingPage} />
-  //               {/* // ? About us overview */}
-  //               <Route path="/about-us" exact component={AboutUs} />
-  //               <Route path="/verify-email" exact component={Verification} />
-  //               <Route path="/user-signed-up" exact component={CheckMail} />
-  //               <Route path="/location-cards" exact component={LocationCards} />
-  //               <Route path="/welcome-page" exact component={Welcome} />
-  //               <Route
-  //                 path="/location-details"
-  //                 exact
-  //                 component={LocationDetails}
-  //               />
-
-  //               {/* // ? Url redirect to landing page on unknown path */}
-  //               <Redirect to="/" exact />
-  //             </Switch>
-  //           </main>
-  //           {/* // ? Footer lives outside of Main and is only visible on tablet + views */}
-  //           <Footer />
-  //         </Router>
-  //       </AppContext.Provider>
-  //     </div>
 };
 
 export default App;
