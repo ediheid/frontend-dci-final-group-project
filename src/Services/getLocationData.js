5d72124a3e7b978fc70c28c282bb8735dadf894f
Edith-Heidmann <eheidmann@gmail.com>
2022-01-12 13:34:17 +0100

    Removed useEffect for location data and passed into mapView function for onSubmit of form

--- src/Services/getLocationData.js
+++ src/Services/getLocationData.js
@@ -1,13 +1,25 @@
 export const locations = async (setMapEventData) => {
-  // setMapLoading(true);
-  // !!! Dummy for location data linked from backend
-  // !! Events will equal an array of objects
   const res = await fetch("http://localhost:3001/location");
   const events = await res.json();
 
   // console.log("????", events.returnedLocations[0].title);
 
   setMapEventData(events.returnedLocations);
-  // ! Not sure if we will use loader or not? as it may interfere with already existing conditional rendering on the map from Form
-  // setMapLoading(false);
 };
+
+// export const locations = async (setMapEventData) => {
+//   const res = await fetch("http://localhost:3001/location");
+
+//   let events;
+
+//   try {
+//     events = await res.json();
+
+//     // console.log("????", events.returnedLocations[0].title);
+//   } catch (error) {
+//     console.log("Error", error);
+//     alert(error);
+//   }
+
+//   setMapEventData(events.returnedLocations);
+// };
