8ecdfedee3178903cac50a6f911e4fc89465fa8d
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2022-01-12 12:55:56 +0100

    initial commit

---
+++ src/Services/getLocationData 2.js
@@ -0,0 +1,13 @@
+export const locations = async (setMapEventData) => {
+  // setMapLoading(true);
+  // !!! Dummy for location data linked from backend
+  // !! Events will equal an array of objects
+  const res = await fetch("http://localhost:3001/location");
+  const events = await res.json();
+
+  console.log("????", events.returnedLocations[0].title);
+
+  setMapEventData(events.returnedLocations);
+  // ! Not sure if we will use loader or not? as it may interfere with already existing conditional rendering on the map from Form
+  // setMapLoading(false);
+};
