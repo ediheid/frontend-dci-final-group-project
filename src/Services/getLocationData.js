d8d6b908f68025e8c13fa1a4f244277d8f684a26
Edith-Heidmann <eheidmann@gmail.com>
2022-01-18 14:26:36 +0100

    Updated cards and footer

--- src/Services/getLocationData.js
+++ src/Services/getLocationData.js
@@ -1,7 +1,7 @@
-const getLocationDataURL = process.env.REACT_APP_GET_LOCATION_DATA_URL;
+const backendURL = process.env.REACT_APP_GET_BACKEND_URL;
 
 export const locations = async (setMapEventData) => {
-  const res = await fetch(getLocationDataURL);
+  const res = await fetch(`${backendURL}location`);
   const events = await res.json();
 
   // console.log("????", events.returnedLocations[0].title);
